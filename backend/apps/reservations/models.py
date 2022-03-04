3d4bc2ce0b497c9ffd1ac08bc1f1bfe7da14590b
Dominik Bullo <dominik@bullo.sk>
2022-03-04 08:40:15 +0100

    feat(backend): add spot to spot reservations

--- backend/apps/reservations/models.py
+++ backend/apps/reservations/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from apps.users.models import User
+from apps.workspaces.models import Spot
 
 
 class Reservation(models.Model):
@@ -13,7 +14,8 @@ class Reservation(models.Model):
 class SpotReservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='created_spot_reservations', on_delete=models.CASCADE)
-    reservation_for = models.ForeignKey(User, related_name='reservations_for_spot', on_delete=models.CASCADE)
+    reservation_for = models.ForeignKey(User, related_name='user_reservations_for_spot', on_delete=models.CASCADE)
     reservation = models.ForeignKey(Reservation, related_name='reservations_for_spot', on_delete=models.CASCADE)
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE)
     permanent = models.BooleanField(default=False)
     frequency = models.IntegerField(blank=True, null=True)
